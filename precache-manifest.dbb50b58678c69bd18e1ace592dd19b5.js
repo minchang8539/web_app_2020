7a1bea777566c937b88d8f5b6a1b9e25b88ae1a5
minchang8539 <no8539@naver.com>
2020-07-13 12:33:15 +0900

    Updates

---
+++ precache-manifest.dbb50b58678c69bd18e1ace592dd19b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e18efabd012af5e4cb2a5e99dae2ebb",
+    "url": "/web_app_2020/index.html"
+  },
+  {
+    "revision": "35e98bdc9b60032a2e1c",
+    "url": "/web_app_2020/static/css/main.7424c902.chunk.css"
+  },
+  {
+    "revision": "7d4124757e0f6f2dfbfd",
+    "url": "/web_app_2020/static/js/2.9ef5f475.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/web_app_2020/static/js/2.9ef5f475.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35e98bdc9b60032a2e1c",
+    "url": "/web_app_2020/static/js/main.ade25284.chunk.js"
+  },
+  {
+    "revision": "eea1e260969effa16869",
+    "url": "/web_app_2020/static/js/runtime-main.5bb0d4cd.js"
+  }
+]);
